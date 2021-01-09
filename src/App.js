096c207ad2f7180211a31c0ee13a08e495464d26
AugustaElle <68307752+AugustaElle@users.noreply.github.com>
2021-01-09 18:45:22 -0500

    Added link to Open source code

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Weather application</h1>
+      <footer>Coded by Leah L.  See my open source code on <a href="https://github.com/AugustaElle/my-app." target="_blank" rel="noopener noreferrer"> Github </a></footer>
     </div>
   );
 }
