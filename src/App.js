f2d7618c49b3cfc86046a890749ef178cc990489
AugustaElle <68307752+AugustaElle@users.noreply.github.com>
2021-01-09 19:16:31 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather application</h1>
       <footer>Coded by Leah L.  See my open source code on <a href="https://github.com/AugustaElle/my-app." target="_blank" rel="noopener noreferrer"> Github </a></footer>
-    </div>
+    </div></div>
   );
 }
 
